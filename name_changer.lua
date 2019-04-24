92d899cdd63f702d3d6a3ca260b560f5039ec644
Artemis <Osmosis.orb@gmail.com>
2019-04-24 22:49:33 +0100

    Fix victim steal, v1 of teammate name steal

--- name_changer.lua
+++ name_changer.lua
@@ -1,19 +1,26 @@
+math.randomseed(client.timestamp())
 local bool_enabled = false
 local bool_first = true
 local client_userid_to_entindex = client.userid_to_entindex
 local entity_get_player_name = entity.get_player_name
 local entity_get_local_player = entity.get_local_player
+local ui_set = ui.set
+local ui_ref = ui.reference
+local steal_name = ui_ref("MISC", "Miscellaneous", "Steal player name")
 
-local function sanitize_string(string)
-    return string:gsub("%W", "")
-end
+local ui_button = ui.new_button("Lua", "B", "test", function() --waits until round start to enable in order to prevent it breaking comp gamemodes
+    bool_enabled = false
+    if bool_enabled then
+        client.log("Enabled")
+    end
+end)
 
-local ui_button = ui.new_button("Lua", "B", "Enable Victim Name Stealer (Comp/Cas)", function() --waits until round start to enable in order to prevent it breaking comp gamemodes
+local ui_button = ui.new_button("Lua", "B", "Steal Teammates Name (Comp/Cas)", function() --waits until round start to enable in order to prevent it breaking comp gamemodes
     bool_enabled = true
     client.log("Name Stealer Enabled - Comp/Casual")
 end)
 
-local ui_button = ui.new_button("Lua", "B", "Enable Victim Name Stealer (Other)", function()
+local ui_button = ui.new_button("Lua", "B", "Steal Teammates Name (Other)", function()
     bool_enabled = true
     client.log("Name Stealer Enabled - (Other)")
     cvar.name:invoke_callback(0)
@@ -35,13 +42,9 @@ local function on_round_start(e)
 end
 
 local function on_player_death(e)
-	local victim_entindex = client_userid_to_entindex(e.userid)
 	local attacker_entindex = client_userid_to_entindex(e.attacker)
     if attacker_entindex == entity_get_local_player() and bool_enabled then
-        local victim_name = entity_get_player_name(victim_entindex)
-        victim_name = sanitize_string(victim_name)
-        client.log("Killed "..victim_name.." stealing their name")
-        cvar.name:set_string(victim_name.." ")
+        ui_set(steal_name, true)
     end
 end
 
